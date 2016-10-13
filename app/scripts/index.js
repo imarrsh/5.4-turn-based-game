da99ee5824b8f38970cd4ad200a403af4265b709
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-13 17:26:27 -0400

    starting charscreen and setting up tests folder

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,15 @@
+var $ = require('jquery');
+var Handlebars = require('handlebars');
+var startPage = require('../templates/start-screen.hbs');
+var charScreen = require('../templates/char-screen.hbs');
 
+
+var gameScreen = $('.page');
+
+gameScreen.html(startPage());
+
+
+$( ".start" ).on( "click", function(event){
+  event.preventDefault();
+  gameScreen.html(charScreen());
+});
---
+++ app/tests/tests.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var expect = chai.expect;
+var $ = require('jquery');
+//
+// describe('Create post form', function(){
+//   it('should trigger a create:post event on the document with the title and body', function(done){
+//     $(document).on('create:post', function(event, posts){
+//       var firstPost = posts[0];
+//       expect(firstPost).to.have.property('title');
+//       expect(firstPost).to.have.property('body');
+//       done();
+//     });
+//     $('.title').val('title');
+//     $('.body').val('body');
+//     $('.button').click();
+//   });
+// })
+
+describe('game start screen', function(){
+  it('should expect user to click start button', function(){
+    
+  })
+})
