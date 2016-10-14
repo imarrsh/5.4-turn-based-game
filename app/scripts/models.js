8d440cf9c2190165c8197c4f7d2387126ec971d2
Marshall Thompson <marshallthmomp@gmail.com>
2016-10-13 22:54:32 -0400

    added some more character data

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -43,19 +43,39 @@ var goodGuys = [
     weapon : {
       kind: 'bed post',
       maxDamage: 10,
-      minDamage: 2,
-    },
+      minDamage: 2
+    }
   }),
   new GoodGuy({
-    name : 'Ron Burgundy',
-    picture: '../images/ron-burgundy.jpg', 
+    name : 'Brick Tamland',
+    picture: '../images/brick-tamland.jpg',
     health : 100,
     weapon : {
-      kind: 'bed post',
-      maxDamage: 10,
-      minDamage: 2,
-    },
+      kind: 'grenade',
+      maxDamage: 25,
+      minDamage: 10
+    }
   }),
+  new GoodGuy({
+    name : 'Brian Fantana',
+    picture: '../images/brian-fantana.jpg',
+    health : 100,
+    weapon : {
+      kind: 'revolver',
+      maxDamage: 18,
+      minDamage: 12
+    }
+  }),
+  new GoodGuy({
+    name : 'Champ Kind',
+    picture: '../images/champ-kind.jpg',
+    health : 100,
+    weapon : {
+      kind: 'brass knuckles',
+      maxDamage: 18,
+      minDamage: 12
+    }
+  })
 ];
 
 
@@ -70,6 +90,19 @@ function Villain(config){
 
 Villain.prototype = new Character();
 
+
+var badGuys = [
+  new Villain({
+    name : 'Wes Mantooth',
+    picture: '../images/wes-mantooth.jpg',
+    health : 100,
+    weapon : {
+      kind: 'switchblade',
+      maxDamage: 15,
+      minDamage: 7
+    }
+  })
+];
 
 
 
